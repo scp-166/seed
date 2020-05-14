76fb962a893b90a8228022da184b997843b497bd
scp-166 <lyl5913@163.com>
2020-05-14 10:09:12 +0800

    fix

--- src/main/java/com/seed/MyApplicationStarter.java
+++ src/main/java/com/seed/MyApplicationStarter.java
@@ -1,8 +1,10 @@
-package com.seed.module;
+package com.seed;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.seed.module.dal")
 @SpringBootApplication
 public class MyApplicationStarter {
     public static void main(String[] args) {
